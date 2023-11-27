48c97be22ab52a35838db1f28ad60a5869d7fe2d
nuggetcrab <johnswift@live.com>
2023-11-26 21:44:35 -0600

    Add files via upload

---
+++ Patches/MeowPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MeowMod.Patches
+{
+    [HarmonyPatch(typeof(MouthDogAI))]
+    internal class MeowPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void soundPatch(ref AudioClip ___breathingSFX)
+        {
+            AssetBundle meowBundle = AssetBundle.LoadFromMemory(MeowMod.Properties.Resources.meowbundle);
+            AudioClip meow = meowBundle.LoadAsset<AudioClip>("Assets/meow.mp3");
+
+            ___breathingSFX = meow;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using BepInEx.Configuration;
+using GameNetcodeStuff;
+using MeowMod.Patches;
+
+namespace MeowMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MeowModBase : BaseUnityPlugin
+    {
+        const string modGUID = "Aries.MeowMod";
+        const string modName = "Mouth Dog Meow";
+        const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static MeowModBase Instance;
+
+        internal ManualLogSource val;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            val = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            val.LogWarning("ICD is online!");
+
+            harmony.PatchAll(typeof(MeowModBase));
+            harmony.PatchAll(typeof(MeowPatch));
+
+        }
+
+    }
+
+}
